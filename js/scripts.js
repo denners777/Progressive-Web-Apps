b801cee9ba5bca1068a5649a0b473df61f56b614
Denner Fernandes <denners777@hotmail.com>
2017-11-22 23:39:04 -0200

    Manipulando eventos do Dom
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-console.log('[App] started');
+let notes = { data: [] }
+
+let createNote = function() {
+    let input = document.querySelector('#form-add-note input[type="text"]');
+    let value = input.value;
+
+    notes.data.push(value);
+
+    input.value = '';
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    let formAddNotes = document.getElementById('form-add-note');
+    formAddNotes.addEventListener('submit', function(e) {
+        e.preventDefault();
+        createNote();
+    });
+});
+
+document.addEventListener('click', function(e) {
+    let notesTag = document.getElementById('notes');
+    if (e.target.parentElement === notesTag) {
+        if (confirm('Devolver esta nota?')) {
+            console.log('ok');
+        }
+    }
+});
