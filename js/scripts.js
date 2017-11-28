894cb6b608077c54619e8962510443d79a109ad7
Denner Fernandes <denners777@hotmail.com>
2017-11-27 22:57:50 -0200

    Erro array.observe
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,49 @@
-let notes = { data: [] }
+let notes = { data: [] };
+
+let updateList = function() {
+    console.log('[Application] start watch');
+
+    Array.observe(notes.data, function(changes) {
+        let index = null;
+        let value = '';
+        let status = null;
+
+        if (changes[0].type === 'splice') {
+            index = changes[0].index;
+            value = changes[0].object[index];
+            status = (changes[0].addedCount > 0) ? 'created' : 'removed';
+        }
+
+        if (changes[0].type === 'update') {
+            index = changes[0].name;
+            value = changes[0].object[index];
+            status = 'updated';
+        }
+
+        if (!value && status === 'created' && status === 'update') {
+            return;
+        }
+
+        let notesTag = document.getElementById('notes');
+
+        if (status === 'update') {
+            console.log('implementar');
+        }
+
+        if (status === 'removed') {
+            let listOfNotes = document.querySelectorAll('#notes li');
+            notesTag.removeChild(listOfNotes[index]);
+        }
+
+        if (status === 'update') {
+            let newLi = document.createElement('li');
+            newLi.innerHTML = value;
+            notesTag.appendChild(newLi);
+        }
+
+
+    });
+}
 
 let createNote = function() {
     let input = document.querySelector('#form-add-note input[type="text"]');
@@ -8,6 +53,8 @@ let createNote = function() {
 
     input.value = '';
 }
+
+updateList();
 
 document.addEventListener('DOMContentLoaded', function(event) {
     let formAddNotes = document.getElementById('form-add-note');
@@ -21,7 +68,12 @@ document.addEventListener('click', function(e) {
     let notesTag = document.getElementById('notes');
     if (e.target.parentElement === notesTag) {
         if (confirm('Devolver esta nota?')) {
-            console.log('ok');
+            let listOfNotes = document.querySelector('#notes li');
+            listOfNotes.forEach(function(item, index) {
+                if (e.target == item) {
+                    notes.data.splice(index, 1);
+                }
+            });
         }
     }
 });
